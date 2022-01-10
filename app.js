019bdf583475e9776131795351e8f8542fa28ccd
Firas00002 <91484251+Firas00002@users.noreply.github.com>
2022-01-10 10:03:44 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require('http')
+const fs=    require('fs')
+const port = 3001
+
+const server = http.createServer(function(req,res){
+res.writeHead(200,{'Content-Type':'text/html'})
+fs.readFile('index.html',function(error,data){
+if(error){
+    res.writeHead(404)
+    res.write('Error: File Not Found ')
+    
+} else{
+    res.write(data)
+}
+res.end()
+})
+
+
+
+})
+
+server.listen(port,function(error){
+    if(error){
+        console.log('error detected',error)
+    } else{
+        console.log('server run ' + port)
+    }
+})
